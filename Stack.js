31a60dff03d31262df6a632645b131ec5ef2fcc6
Silas Burger <ybsilas@gmail.com>
2018-12-17 21:27:00 -0800

    finished stack implemented with array

--- Stack.js
+++ Stack.js
@@ -1,6 +1,6 @@
 const LinkedList = require('./linkedList')
 
-class Stack {
+class linkedListStack {
   constructor () { 
     this.myList = new LinkedList();
   }
@@ -26,4 +26,30 @@ class Stack {
   }
 }
 
-module.exports = Stack;
+class arrayStack {
+  constructor () { 
+    this.myList = new Array();
+  }
+
+  push(item) {
+    this.myList.push(item);
+  }
+
+  pop() {
+    const removed = this.myList.pop();
+    if(removed===undefined) {
+      const err = new Error('The list is already empty');
+      throw err;
+    }
+  }
+
+  peek() {
+    return this.myList[this.mylist.length - 1];
+  }
+
+  isEmpty() {
+    return Boolean(this.mylist.length);
+  }
+}
+
+module.exports = linkedListStack, arrayStack;
