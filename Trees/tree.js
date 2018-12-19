40fe8539eb6d03b2c1afa7e3ac702ead8813ba1d
Silas Burger <ybsilas@gmail.com>
2018-12-19 11:04:28 -0800

    implementing tree

--- Trees/tree.js
+++ Trees/tree.js
@@ -1,13 +1,24 @@
 class Tree {
-  constructor() {
-    
+  constructor(root) {
+    this.root = root;
   }
-
+  findInTree(val) {
+    this.root.find(val)
+  }
+  findInTreeBFS(val) {
+    this.root.findBFS(val);
+  }
 }
 
 class Node {
-  constructor() {
-    
+  constructor(parent) {
+    this.parent = parent;
   }
 
+  find(val) {
+    
+  }
+  findBFS(val) {
+
+  }
 }
