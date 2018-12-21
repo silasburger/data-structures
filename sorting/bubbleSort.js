6ebe2f2d5a318a4e68135433ac7a8e1a5502d284
Silas Burger <ybsilas@gmail.com>
2018-12-21 14:27:33 -0800

    working on merge

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,16 +1,16 @@
 function bubbleSort(array) {
   if(array.length <= 1) return array;
-  let pointer1 = 0;
-  while(array[pointer1] !== undefined) {
-    for(let i = pointer1; i < array.length-1; i++) {
-      if(array[i] > array[i+1]) {
-        pointer1++;
-        break;
-      }
+  let hadToSort = true;
+  let j = 0;
+  while(hadToSort) {
+    hadToSort = false;
+    for(let i = 0; i < array.length-1-j; i++) {
       if(array[i] > array[i+1]) {
         [array[i], array[i+1]] = [array[i+1], array[i]];
+        hadToSort = true;
       }
     }
+    j++;
   }
   return array;
 }
---
+++ sorting/merge.js
@@ -0,0 +1,14 @@
+function merge(arr1, arr2) {
+  pointer1 = 0;
+  pointer2 = 0;
+  sortedArr = [];
+  while(sorted.length < arr1.length + arr2.length) {
+    if(arr1[pointer1] < arr2[pointer2]) {
+      sortedArr.push(arr1[pointer1]);
+      pointer1++;
+    }
+    if(arr1[pointer1] < arr2[pointer2]) {
+
+    }
+  }
+}
