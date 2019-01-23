8f2514850937c2be013cac7493c25846a5366ba7
Silas Burger <ybsilas@gmail.com>
2019-01-22 16:08:58 -0800

    made changes to find-rotated-index

--- problem-solving/prob-divide-conquer/find-rotated-index.js
+++ problem-solving/prob-divide-conquer/find-rotated-index.js
@@ -9,15 +9,19 @@ findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12) // -1
 function findRotatedIndex(rArr, target) {
   let p1 = 0;
   let p2 = rArr.length - 1;
-  let curr = Math.floor((p1 + p2 / 2));
-  while(rArr[curr] !== target && p1 <= p2) {
+  let curr = Math.floor((p1 + p2) / 2);
+
+  while(p1 <= p2) {
     if(rArr[curr] === target) return curr;
-    if(target < rArr[curr] && target > rArr[p1]) p2 = curr - 1;
-    if(target < rArr[curr] && target < rArr[p1]) p1 = curr + 1; 
-    if(target > rArr[curr] && target < rArr[p2]) p1 = curr + 1;
-    if(target > rArr[curr] && target > rArr[p2]) p2 = curr - 1;
+    if(target < rArr[curr] && target >= rArr[p1]) p2 = curr - 1;
+    else if(target < rArr[curr] && target <= rArr[p1]) p1 = curr + 1; 
+    else if(target > rArr[curr] && target <= rArr[p2]) p1 = curr + 1;
+    else if(target > rArr[curr] && target >= rArr[p2]) p2 = curr - 1;
+    curr = Math.floor((p1 + p2) / 2);
+
   }
   return -1;
 }
+
 
 module.exports = findRotatedIndex
