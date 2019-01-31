f2445d18c22b811f21356f2867ca45052625b05c
Silas Burger <ybsilas@gmail.com>
2019-01-31 12:44:50 -0800

    solved longest fall

--- problem-solving/prob-freq-counter-pointers-ex/construct-note.js
+++ problem-solving/prob-freq-counter-pointers-ex/construct-note.js
@@ -1,4 +1,22 @@
 // add whatever parameters you deem necessary
-function constructNote() {}
+function constructNote(message, letters) {
+  let freqCounter = {};
+  for(let letter of letters) {
+    freqCounter[letter] = (freqCounter[letter] ? freqCounter[letter] : 0) + 1;
+  }
+
+  for(let letter of message) {
+    freqCounter[letter] = (freqCounter[letter] ? freqCounter[letter] : 0) - 1;
+    if(freqCounter[letter] < 0) return false  
+  }
+
+  return true;
+}
 
 module.exports = constructNote;
+
+// loop through letters and add to frequency counter
+// loop through message and remove letters from frequency counter
+// if counter goes below zero
+  //return false
+//return true
--- problem-solving/prob-freq-counter-pointers-ex/longest-fall.js
+++ problem-solving/prob-freq-counter-pointers-ex/longest-fall.js
@@ -1,4 +1,34 @@
 // add whatever parameters you deem necessary
-function longestFall() {}
+function longestFall(numArr) {
+  if (numArr.length === 0) return 0;
+  if (numArr.length === 1) return 1;
+
+  let pointer1 = 0;
+  let pointer2 = 0;
+  let longestFall = 0;
+  let fall;
+
+  while (pointer2 < numArr.length - 1) {
+    if (numArr[pointer2 + 1] < numArr[pointer2]) {
+      pointer2++;
+      fall = pointer2 - pointer1 + 1;
+    } else {
+      pointer1++;
+      pointer2 = pointer1;
+    }
+    if (fall > longestFall) {
+      longestFall = fall;
+    }
+  }
+  return longestFall;
+}
+// initialize pointer1 and pointer2 at 0 and 1, respectively
+// initialize longestFall at 0
+
+// while pointer2 is less than the length of the array
+// check if pointer2 is less than pointer1
+// set longestFall to pointer2 - pointer1 and then iterate pointer2
+// else iterate both pointer1 and pointer2
+// return longestFall
 
 module.exports = longestFall;
