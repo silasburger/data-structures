d540c531c1103e1cb32babec75cbf57d7f29a827
Silas Burger <ybsilas@gmail.com>
2019-02-03 09:52:28 -0800

    did pivotIndex

--- problem-solving/prob-freq-counter-pointers-ex/pivot-index.js
+++ problem-solving/prob-freq-counter-pointers-ex/pivot-index.js
@@ -1,4 +1,48 @@
 // add whatever parameters you deem necessary
-function pivotIndex() {}
+// expect(pivotIndex([1, 2, 1, 6, 3, 1])).toBe(3); // 3
+
+// p1, p2, sum1, sum2
+// if sums are equal add one to both sides
+// if p1 and p2 are on same index and sums are equal, then return idx
+// if sums are not equal, then move
+
+function pivotIndex(arr) {
+  const sum = arr.reduce(function(acc, val) {
+    acc += val;
+    return acc;
+  }, 0);
+  let p1 = 0;
+  let p2 = arr.length - 1;
+  let sum1 = arr[p1];
+  let sum2 = arr[p2];
+  let remainder;
+
+  while (p1 < p2) {
+    remainder = sum - (sum1 + sum2);
+    if (sum1 > sum2 && remainder > 0) {
+      p2++;
+      sum2 += arr[p2];
+    } else if (sum2 > sum1 && remainder > 0) {
+      p1++;
+      sum1 += arr[p1];
+    } else if (sum1 > sum2 && remainder < 0) {
+      p1++;
+      sum1 += arr[p1];
+    } else if (sum2 > sum1 && remainder < 0) {
+      p2++;
+      sum2 += arr[p2];
+    } else if (sum1 === sum2) {
+      p1++;
+      p2--;
+      sum2 += arr[p2];
+      sum1 += arr[p1];
+    } else if(remainder === 0) {
+      p2--;
+      sum2 += arr[p2];
+  }
+    if (sum1 === sum2 && p1 === p2) return p1;
+  }
+  return -1;
+}
 
 module.exports = pivotIndex;
--- problem-solving/prob-freq-counter-pointers-ex/two-array-object.js
+++ problem-solving/prob-freq-counter-pointers-ex/two-array-object.js
@@ -1,4 +1,28 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {}
+function twoArrayObject(keysArr, valsArr) {
+  const object = {};
+  let pointer = 0;
+  let val;
+
+  while(keysArr[pointer] !== undefined ) {
+    val = valsArr[pointer] === undefined ? null : valsArr[pointer];
+    object[keysArr[pointer]] = val;
+    pointer++;
+  }
+
+  return object;
+}
 
 module.exports = twoArrayObject;
+
+//must be letter in first array
+//second array cant be longeer than first but first can be longer than second
+
+//twoArrayObjest(['a', 'b', 'c', 'd'], [1, 2, 3]) => {a:1, b:1, c:3, d:null}
+
+
+// one pointer starts at 0
+// while keyArr[pointer] isn't undefined loop
+  // if valsArr[pointer] is undefined, val is null
+  // add key of keyArr[pointer] and value of val to object
+// return object
